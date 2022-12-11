9c7c0a96b1b308269fd31b975d420ba9f70a6ba1
Gabi <gabi@MacPro-0169.local>
2022-12-11 09:57:24 -0300

    fix: improvements to the ui and readme

--- app/root.tsx
+++ app/root.tsx
@@ -7,26 +7,29 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
-import styles from "./styles/app.css";
+import styles from "./tailwind.css"
 
 export function links() {
-  return [{ rel: "stylesheet", href: styles }];
+  return [
+      { rel: 'stylesheet', href: styles },
+      { rel: 'stylesheet', href: 'https://rsms.me/inter/inter.css' },
+  ];
 }
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "ADA Handle Starter Kit",
+  title: "Ada Handle Starter Kit",
   viewport: "width=device-width,initial-scale=1",
 });
 
 export default function App() {
   return (
-    <html lang="en">
+    <html lang="en" className="dark">
       <head>
         <Meta />
         <Links />
       </head>
-      <body className="w-screen h-screen bg-gray-800">
+      <body className="app w-full h-screen flex flex-col bg-gray-950 overflow-y-scroll">
         <Outlet />
         <ScrollRestoration />
         <Scripts />
